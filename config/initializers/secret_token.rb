a3d49acf5d40eca9d84d7b966130e1735317bb98
sinfinale <sinfin_ale@hotmail.com>
2013-08-12 00:17:06 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Servicio7::Application.config.secret_token = 'cef05e283d958dff8fc20c2d1bb44c11c19583eda932ed8057ae2c6ad4b19c9068204a1a55efd99e7c3160f3bd6e36b376728f1e97a509a779aac15cbeefd0d2'
